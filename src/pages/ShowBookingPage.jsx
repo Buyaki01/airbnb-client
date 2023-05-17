56ac5f60bed5d742d07479432de9ceb7d899e2df
Buyaki01 <rittahbuyaki@gmail.com>
2023-05-17 11:53:30 +0300

    Add AccomodationGallery to use in the DisplayAccomodationDetailsPage and ShowBookingPage component

--- src/pages/ShowBookingPage.jsx
+++ src/pages/ShowBookingPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import AddressLink from "./AddressLink"
 import BookingDates from "./BookingDates"
+import AccomodationGallery from "./AccomodationGallery"
 
 const ShowBookingPage = () => {
   const {id} = useParams()
@@ -27,14 +28,19 @@ const ShowBookingPage = () => {
 
   return(
     <div className="my-8">
-      <h1>{booking.accomodationId.title}</h1>
+      <h1 className="text-3xl">{booking.accomodationId.title}</h1>
       <AddressLink accomodation={booking.accomodationId} className="my-2 block" />
       <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
-         <div>
-          <h2 className="text-2xl mb-4">Your booking information:</h2>
+        <div>
+          <h2 className="text-2xl mb-2">Your booking information:</h2>
           <BookingDates booking={booking}/>
         </div>
+        <div className="bg-primary p-6 text-white rounded-2xl">
+          <div>Total Price</div>
+          <div className="text-2xl">${booking.price}</div>
+        </div>
       </div>
+      <AccomodationGallery accomodation={booking.accomodationId}/>
     </div>
   )
 }
