b942a03962ec5153f71ccf7147062bc3d7f5ab81
Buyaki01 <rittahbuyaki@gmail.com>
2023-05-17 16:43:02 +0300

    Update baseURL and .env file

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,6 +1,6 @@
 const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://api-airbnb-gmqf.onrender.com'
-    : 'http://localhost:4000'
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000'
+    : 'https://api-airbnb-gmqf.onrender.com'
 
 export default baseURL
