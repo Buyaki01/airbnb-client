1315acc46449e1bc5c781fb9e9a00dfa566d0f97
Buyaki01 <rittahbuyaki@gmail.com>
2023-05-19 17:15:55 +0300

    Update baseurl to vercel

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000'
-    : 'https://api-airbnb-gmqf.onrender.com'
+    : 'https://airbnb-api-omega.vercel.app'
 
 export default baseURL
